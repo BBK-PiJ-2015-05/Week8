6126c7c06f098a3b8cd0cbbd7ca045877d87bee2
BBK-PiJ-2015-05 <sconno05@dcs.bbk.ac.uk>
2015-11-18 14:40:34 +0000

    2. mkdir

---
+++ MkDir.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+/**
+* Week 8 Day 16
+* 2. mkdir
+* @author Sarah Connor
+*/
+
+public class MkDir{
+	
+	public static void main(String[] args){
+		
+		for(int i = 0; i < args.length; i++){
+			File file = new File(args[i]);
+			file.mkdir();
+		}
+	}
+}
+
